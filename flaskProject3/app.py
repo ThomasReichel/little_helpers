dd8d36a11d486c980e03d35470642a63539a9113
ThomasReichel <computer@web4tom.de>
2022-02-07 11:36:48 +0100

    pw_label

--- flaskProject3/app.py
+++ flaskProject3/app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+import lib
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return 'Index_Page'
+
+@app.route('/pw_label')
+def pw_label():
+    return render_template('pw_label.html')
+
+@app.route('/pw_print', methods=['POST', 'GET'])
+def new_pw():
+    pw = ""
+    anzahl = 0
+    pw = request.form['pw']
+    anzahl = request.form['anzahl']
+    output = lib.get_outputString(pw)
+    lib.create_img(output)
+    lib.print_label(anzahl)
+    return render_template('pw_print.html', pw=pw, anzahl=anzahl )
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ flaskProject3/lib.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+import time
+from PIL import Image, ImageFont, ImageDraw
+1
+def get_outputString(password):
+    string = "User:\n dcadmin\nPasswort:\n {}".format(password)
+    return string
+
+def get_fonts():
+    fc_list = os.popen("fc-list").read().split("\n")
+    ret = []
+    for fc in fc_list:
+        match = re.match(r".*\/(.*\.ttf)", fc)
+        if(match):
+            ret.append(match.groups(0)[0])
+    return ret
+
+
+
+def create_img(output):
+    # Label erstellen
+    img = Image.new('1', (614, 283), color='white')
+    d = ImageDraw.Draw(img)
+    fonts = get_fonts()
+    fontPath = 'NotoSansMono-Regular.ttf'
+    if (fontPath not in fonts):
+        fontPath = [i for i in fonts if ("mono" in i.lower())][0]
+    font = ImageFont.truetype(fontPath, 40)
+    d.text((100, 5), output, font=font, fill='black')
+    img.save('new_pw.bmp')
+    return 0
+
+
+def print_label(anzahl):
+    try:
+        for i in range(int(anzahl)):
+            os.system('lp new_pw.bmp')
+            time.sleep(1)
+            while (os.popen("lpstat").read()):
+                time.sleep(0.2)
+        print("Programm erfolgreich beendet")
+
+    except:
+        print("Drucker nicht angeschlossen!")
+
+    return 0
